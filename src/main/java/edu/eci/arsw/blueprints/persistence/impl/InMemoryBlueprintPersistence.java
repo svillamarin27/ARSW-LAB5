9a4b553e1b163b9618705fb817d3936553ec8cba
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-02-18 10:15:06 -0500

    parte 1-3

--- src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -26,16 +26,26 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
     private final Map<Tuple<String,String>,Blueprint> blueprints=new HashMap<>();
     
     public InMemoryBlueprintPersistence() {
-        //load stub data
-        Point[] pts=new Point[]{new Point(140, 140),new Point(115, 115)};
-        Blueprint bp=new Blueprint("_authorname_", "_bpname_ ",pts);
-        blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        //plano 1
+        Point[] point1=new Point[]{new Point(140, 140),new Point(115, 115)};
+        Blueprint bp1=new Blueprint("Daniel", "bp1 ",point1);
+        blueprints.put(new Tuple<>(bp1.getAuthor(),bp1.getName()), bp1);
+        // plano 2
+        Point[] point2=new Point[]{new Point(12, 13),new Point(14, 15)};
+        Blueprint bp2=new Blueprint("Sebastian", "bp2 ",point2);
+        blueprints.put(new Tuple<>(bp2.getAuthor(),bp2.getName()), bp2);
+        // plano 3
+        Point[] point3=new Point[]{new Point(26, 27),new Point(28, 29)};
+        Blueprint bp3=new Blueprint("Jorge", "bp3 ",point3);
+        blueprints.put(new Tuple<>(bp3.getAuthor(),bp3.getName()), bp3);
+        
         
     }    
     
     @Override
     public void saveBlueprint(Blueprint bp) throws BlueprintPersistenceException {
         if (blueprints.containsKey(new Tuple<>(bp.getAuthor(),bp.getName()))){
+        	
             throw new BlueprintPersistenceException("The given blueprint already exists: "+bp);
         }
         else{
@@ -45,7 +55,11 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
 
     @Override
     public Blueprint getBlueprint(String author, String bprintname) throws BlueprintNotFoundException {
-        return blueprints.get(new Tuple<>(author, bprintname));
+    	Blueprint blpr=blueprints.get(new Tuple<>(author, bprintname));
+    	if(blpr==null) {
+    		throw new BlueprintNotFoundException("Este plano esta mal");
+    	}
+        return blpr;
     }
 
     @Override
